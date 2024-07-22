2bc360a99cda126c5d704206e201f0990f67f4dc
bensonc <craig.benson@ppb.com>
2024-07-22 17:56:52 +0100

    Adding @builder, @Data, AllArgsConstructor to Sensordata

--- weather-service/src/main/java/com/demo/weather_service/data/SensorData.java
+++ weather-service/src/main/java/com/demo/weather_service/data/SensorData.java
@@ -6,9 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
 public class SensorData {
 
   @Id
@@ -34,53 +39,5 @@ public class SensorData {
     this.temperature = temperature;
     this.humidity = humidity;
     this.windSpeed = windSpeed;
-  }
-
-  public String getSensorId() {
-    return sensorId;
-  }
-
-  public void setSensorId(String sensorId) {
-    this.sensorId = sensorId;
-  }
-
-  public LocalDate getDate() {
-    return date;
-  }
-
-  public void setDate(LocalDate date) {
-    this.date = date;
-  }
-
-  public double getTemperature() {
-    return temperature;
-  }
-
-  public void setTemperature(Double temperature) {
-    this.temperature = temperature;
-  }
-
-  public double getHumidity() {
-    return humidity;
-  }
-
-  public void setHumidity(Double humidity) {
-    this.humidity = humidity;
-  }
-
-  public double getWindSpeed() {
-    return windSpeed;
-  }
-
-  public void setWindSpeed(Double windSpeed) {
-    this.windSpeed = windSpeed;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Long getId() {
-    return id;
   }
 }
