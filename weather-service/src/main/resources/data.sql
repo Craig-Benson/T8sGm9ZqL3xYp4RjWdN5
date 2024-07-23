36c2a0e1f4d51f835883aaa9066904f69abb6d72
bensonc <craig.benson@ppb.com>
2024-07-23 14:34:14 +0100

    updated db entries to be within timeframe

--- weather-service/src/main/resources/data.sql
+++ weather-service/src/main/resources/data.sql
@@ -3,16 +3,16 @@ insert into sensor_data
 values(10000, 'sensor-1','2024-07-17', 10, 60.0, 5.0);
 insert into sensor_data
 (id,sensor_id,date,temperature,humidity,wind_speed)
-values(10001, 'sensor-1','2024-08-17', 5, 90.0, 1.0);
+values(10001, 'sensor-1','2024-07-17', 5, 90.0, 1.0);
 insert into sensor_data
 (id,sensor_id,date,temperature,humidity,wind_speed)
-values(10002, 'sensor-2','2024-08-17', 15, 20.0, 6.0);
+values(10002, 'sensor-2','2024-07-17', 15, 20.0, 6.0);
 insert into sensor_data
 (id,sensor_id,date,temperature,humidity,wind_speed)
-values(10003, 'sensor-2','2024-08-17', 15, 20.0, 6.0);
+values(10003, 'sensor-2','2024-07-17', 15, 20.0, 6.0);
 insert into sensor_data
 (id,sensor_id,date,temperature,humidity,wind_speed)
-values(10004, 'sensor-3','2024-08-17', 1, 0.0, 15.0);
+values(10004, 'sensor-3','2024-07-17', 1, 0.0, 15.0);
 insert into sensor_data
 (id,sensor_id,date,temperature,humidity,wind_speed)
-values(10005, 'sensor-3','2024-08-17', -2, 10.0, 15.0);
+values(10005, 'sensor-3','2024-07-17', -2, 10.0, 15.0);
