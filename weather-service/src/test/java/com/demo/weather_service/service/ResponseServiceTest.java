e6d33ca3f6ed62cb8a31f90a0aac8502b42a8734
bensonc <craig.benson@ppb.com>
2024-07-23 14:30:47 +0100

    refactors to ResponseServiceTest

--- weather-service/src/test/java/com/demo/weather_service/service/ResponseServiceTest.java
+++ weather-service/src/test/java/com/demo/weather_service/service/ResponseServiceTest.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.demo.weather_service.data.SensorData;
 import com.demo.weather_service.fixtures.SensorDataFixture;
-import java.util.HashMap;
+import com.demo.weather_service.response.ApiResponse;
+import com.demo.weather_service.response.SensorDataResponse;
+import com.demo.weather_service.response.SensorDatasResponse;
+import com.demo.weather_service.response.StatisticsResponse;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -22,48 +25,55 @@ class ResponseServiceTest {
 
   @Test
   void ShouldReturnOkResponseWhenSensorDataIsValid() {
-    ResponseEntity<SensorData> expected = ResponseEntity.ok(SensorDataFixture.getSensorData());
-    assertEquals(expected, responseService.createLatestStateResponse(SensorDataFixture.getSensorData()));
+
+    ResponseEntity<SensorDataResponse> expected = ResponseEntity.ok().body(new SensorDataResponse(SensorDataFixture.getSensorData()));
+    assertEquals(expected,
+        responseService.createLatestStateResponse(SensorDataFixture.getSensorData()));
   }
 
   @Test
   void ShouldReturnOkResponseWhenSensorDataMapContainsValidSensorData() {
-    Map<String, SensorData> sensorDataMap = Map.of("sensor-1", SensorDataFixture.getSensorData(), "sensor-2", SensorDataFixture.getSensorData());
-    ResponseEntity<Map<String, SensorData>> expected = ResponseEntity.ok(sensorDataMap);
-    assertEquals(expected, responseService.createMultipleSensorDataResponse(sensorDataMap));
+    Map<String, SensorData> sensorDataMap = SensorDataFixture.getSensorDataMap();
+    ResponseEntity<ApiResponse> expected = ResponseEntity.ok(new SensorDatasResponse(sensorDataMap));
+    assertEquals(expected, responseService.createLatestStatesResponse(sensorDataMap));
   }
 
   @Test
-  void whenSensorDataMapIsValidShouldReturnOkResponse() {
-    Map<String, SensorData> sensorDataMap = new HashMap<>();
-    sensorDataMap.put("sensor-1", SensorDataFixture.getSensorData());
-    sensorDataMap.put("invalid", null);
-    ResponseEntity<Map<String, SensorData>> expected = ResponseEntity.status(HttpStatus.MULTI_STATUS).body(sensorDataMap);
-    assertEquals(expected, responseService.createMultipleSensorDataResponse(sensorDataMap));
+  void whenSensorDataMapContainsValidAndInvalidShouldReturnMultiStatusResponse() {
+    ResponseEntity<ApiResponse> expected =
+        ResponseEntity.status(HttpStatus.MULTI_STATUS)
+            .body(new SensorDatasResponse(SensorDataFixture.getValidAndInvalidSensorDataMap()));
+
+    assertEquals(expected, responseService.createLatestStatesResponse(
+        SensorDataFixture.getValidAndInvalidSensorDataMap()));
   }
 
   @Test
   void whenNoSensorDataIsPresentShouldReturnNotFoundResponse() {
-    assertEquals(ResponseEntity.notFound().build(), responseService.createMultipleSensorDataResponse(Map.of()));
+    assertEquals(ResponseEntity.notFound().build(),
+        responseService.createLatestStatesResponse(Map.of()));
   }
 
   @Test
   void whenNoStatisticsPresentShouldReturnNotFoundResponse() {
-    assertEquals(ResponseEntity.notFound().build(), responseService.createStatisticsResponse(Map.of()));
+    assertEquals(ResponseEntity.notFound().build(),
+        responseService.createStatisticsResponse(Map.of()));
   }
 
   @Test
   void whenStatisticsContainsInvalidAndValidStatisticsShouldReturnMultiStatusResponse() {
     Map<String, Double> statisticValues = StatisticsFixture.getValidAndInvalidStatistics();
     Map<String, Map<String, Double>> sensorStatistics = Map.of("sensor-1", statisticValues);
-    assertEquals(ResponseEntity.status(HttpStatus.MULTI_STATUS).body(sensorStatistics), responseService.createStatisticsResponse(sensorStatistics));
+    assertEquals(ResponseEntity.status(HttpStatus.MULTI_STATUS).body(new StatisticsResponse(sensorStatistics)),
+        responseService.createStatisticsResponse(sensorStatistics));
   }
 
   @Test
   void whenStatisticsContainsValidStatisticsShouldReturnOkResponse() {
     Map<String, Double> statisticValues = StatisticsFixture.getValidStatistics();
     Map<String, Map<String, Double>> sensorStatistics = Map.of("sensor-1", statisticValues);
-    assertEquals(ResponseEntity.ok().body(sensorStatistics), responseService.createStatisticsResponse(sensorStatistics));
+    assertEquals(ResponseEntity.ok().body(new StatisticsResponse(sensorStatistics)),
+        responseService.createStatisticsResponse(sensorStatistics));
   }
 
 }
