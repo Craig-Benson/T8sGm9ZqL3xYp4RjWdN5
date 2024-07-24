ed57970edb88db1149ddf158fb5d66100091bfa8
bensonc <craig.benson@ppb.com>
2024-07-24 08:29:48 +0100

    Refactored SensorController and added tests

--- weather-service/src/main/java/com/demo/weather_service/controllers/sensor/SensorController.java
+++ weather-service/src/main/java/com/demo/weather_service/controllers/sensor/SensorController.java
@@ -2,37 +2,26 @@ package com.demo.weather_service.controllers.sensor;
 
 import com.demo.weather_service.data.SensorData;
 import com.demo.weather_service.repository.SensorDataRepository;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class SensorController {
 
   private final SensorDataRepository repository;
-
   public SensorController(SensorDataRepository repository) {
     this.repository = repository;
   }
 
   @PostMapping("/weather-service/update")
-  public ResponseEntity<SensorData> save(@RequestBody @Validated SensorData sensorData)
-      throws MalformedURLException, URISyntaxException {
-    SensorData savedData = repository.save(sensorData);
+  public ResponseEntity<SensorData> save(@RequestBody @Validated SensorData sensorData) {
 
-    URI location =
-        ServletUriComponentsBuilder
-            .fromCurrentRequest()
-            .path("/{id}")
-            .build(savedData.getId())
-            .toURL().toURI();
+    repository.save(sensorData);
 
-    return ResponseEntity.created(location).body(savedData);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 }
